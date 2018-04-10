1e5f8a0e880cf11d71818768113ddab12b2ab1d5
zhaomingli <zhaomingli@192.168.1.6>
2018-04-10 10:37:16 +0800

    基础提交

--- src/main/java/com/how2java/tmall/controller/CategoryController.java
+++ src/main/java/com/how2java/tmall/controller/CategoryController.java
@@ -2,12 +2,22 @@ package com.how2java.tmall.controller;
  
 import com.how2java.tmall.pojo.Category;
 import com.how2java.tmall.service.CategoryService;
+import com.how2java.tmall.util.ImageUtil;
+import com.how2java.tmall.util.Page;
+import com.how2java.tmall.util.UploadedImageFile;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
- 
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpSession;
  
 @Controller
 @RequestMapping("")
@@ -16,9 +26,30 @@ public class CategoryController {
     CategoryService categoryService;
   
     @RequestMapping("admin_category_list")
-    public String list(Model model){
-        List<Category> cs= categoryService.list();
+    public String list(Model model,Page page){
+        List<Category> cs= categoryService.list(page);
+        int total = categoryService.total();
+        page.setTotal(total);
         model.addAttribute("cs", cs);
+        model.addAttribute("page",page);
         return "admin/listCategory";
+    }
+    
+    @RequestMapping("admin_category_add")
+    public String add(Category c,HttpSession session,UploadedImageFile uploadedImageFile) throws IOException{
+		categoryService.add(c);
+		File imageFolder = new File(session.getServletContext().getRealPath("img/category"));
+		File file = new File(imageFolder,c.getId() + ".jpg");
+		if(!file.getParentFile().exists()) {
+			file.getParentFile().mkdirs();
+		}
+		uploadedImageFile.getImage().transferTo(file);
+		
+		//将本地图片转码为jpg，解决上面对文件操作可能造成对泛红问题。
+		BufferedImage img = ImageUtil.change2jpg(file);
+		ImageIO.write(img, "jpg", file);
+		
+    		return "redirect:/admin_category_list";
+    		
     }
 }
--- src/main/java/com/how2java/tmall/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/how2java/tmall/service/impl/CategoryServiceImpl.java
@@ -2,6 +2,8 @@ package com.how2java.tmall.service.impl;
 import com.how2java.tmall.mapper.CategoryMapper;
 import com.how2java.tmall.pojo.Category;
 import com.how2java.tmall.service.CategoryService;
+import com.how2java.tmall.util.Page;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -9,8 +11,17 @@ import java.util.List;
 public class CategoryServiceImpl  implements CategoryService {
     @Autowired
     CategoryMapper categoryMapper;
-    public List<Category> list(){
-        return categoryMapper.list();
-    };
+	@Override
+	public int total() {
+		return categoryMapper.total();
+	}
+	@Override
+	public List<Category> list(Page page) {
+		return categoryMapper.list(page);
+	}
+	@Override
+	public void add(Category category) {
+		categoryMapper.add(category);
+	};
  
 }
